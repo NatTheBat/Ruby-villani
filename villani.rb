c8c976de333567bee7bc2a8c42380444f9c368b2
NatTheBat <natacha.agafonov@gmail.com>
2017-10-09 23:32:35 +0200

    villani

---
+++ villani.rb
@@ -0,0 +1,33 @@
+/i = 0
+
+while i < 10
+
+		if i%5==0  || i%3==0
+		
+		puts i
+	end
+		
+i = i + 1
+		
+end
+
+		puts i/
+
+/total = nombre = 0
+10.times do |multiple|
+	if nombre%5==0  || nombre%3==0
+	nombre = nombre + 1
+	total = total + nombre 
+	end
+end
+puts total.to_s /
+somme = 0
+nombre = 0
+for indice in 1..10 do 
+	if nombre%5==0
+		somme = indice + somme
+	end
+
+end
+
+puts somme
